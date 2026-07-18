75d43de639efae68b9fa6965883d58b51ba7de1b
ikas <65895428+ikas-mc@users.noreply.github.com>
2026-07-18 18:23:12 +0800

    support window scale

--- mpv-winui/mpv-winui/MainWindow_Size.cs
+++ mpv-winui/mpv-winui/MainWindow_Size.cs
@@ -1,5 +1,6 @@
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using mpv_winui.Modules.Win32;
 using System;
 using Windows.Graphics;
 
@@ -8,6 +9,7 @@ namespace mpv_winui
     public sealed partial class MainWindow : Window
     {
         private int _x, _y, _w, _h;
+        private double _scale;
         private void SetupWindowSize()
         {
             try
@@ -18,13 +20,14 @@ namespace mpv_winui
                     int[] v = Array.ConvertAll(saved.Split(','), int.Parse);
                     if (v.Length == 4)
                     {
+                        var scale = Win32WindowHelper.GetWindowScale(this);
                         if (v[0] > 0 && v[1] > 0)
                         {
-                            AppWindow.MoveAndResize(new RectInt32(v[0], v[1], Math.Max(100, v[2]), Math.Max(100, v[3])));
+                            AppWindow.MoveAndResize(new RectInt32((int)(v[0] / scale), (int)(v[1] / scale), Math.Max(100, (int)(v[2] / scale)), Math.Max(100, (int)(v[3] / scale))));
                         }
                         else
                         {
-                            AppWindow.Resize(new SizeInt32(Math.Max(100, v[2]), Math.Max(100, v[3])));
+                            AppWindow.Resize(new SizeInt32(Math.Max(100, (int)(v[2] / scale)), Math.Max(100, (int)(v[3] / scale))));
                         }
                     }
                 }
@@ -34,19 +37,38 @@ namespace mpv_winui
                 AppContext.AppLogger.Error(ex, "restore window position and size failed");
             }
 
+            this.Body.Loaded += Body_Loaded;
+            this.Body.Unloaded += Body_Unloaded;
+
             AppWindow.Changed += Size_AppWindow_Changed;
             Closed += Size_Window_Closed;
         }
 
         private void Size_AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
         {
-            //TODO OverlappedPresenterState.Maximized
-            if (sender.Presenter.Kind != AppWindowPresenterKind.FullScreen || sender.Presenter.Kind != AppWindowPresenterKind.Overlapped)
+            if (!args.DidSizeChange && !args.DidPositionChange)
             {
-                _x = sender.Position.X;
-                _y = sender.Position.Y;
+                return;
+            }
+
+            //TODO Maximized
+            if (sender.Presenter.Kind == AppWindowPresenterKind.Overlapped || sender.Presenter.Kind == AppWindowPresenterKind.CompactOverlay)
+            {
+                if (sender.Position.X > 0)
+                {
+                    _x = sender.Position.X;
+                }
+                if (sender.Position.Y > 0)
+                {
+                    _y = sender.Position.Y;
+                }
                 _w = sender.Size.Width;
                 _h = sender.Size.Height;
+            }
+
+            if (AppContext.AppLogger.IsTraceEnabled)
+            {
+                AppContext.AppLogger.Debug("window last rect: x={},y={},w={},h={}. scale={}", _x, _y, _w, _h, _scale);
             }
         }
 
@@ -67,6 +89,41 @@ namespace mpv_winui
             catch (Exception ex)
             {
                 AppContext.AppLogger.Error(ex, "save window position and size failed");
+            }
+        }
+
+        private void Body_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement body)
+            {
+                body.XamlRoot?.Changed += RootGridXamlRoot_Changed;
+                UpdateWindowMinSize(240, 240);
+            }
+        }
+
+        private void Body_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement body)
+            {
+                body.XamlRoot?.Changed -= RootGridXamlRoot_Changed;
+            }
+        }
+
+        private void RootGridXamlRoot_Changed(XamlRoot sender, XamlRootChangedEventArgs args)
+        {
+            UpdateWindowMinSize(240, 240);
+        }
+
+        public void UpdateWindowMinSize(int w, int h)
+        {
+            if (Content.XamlRoot is XamlRoot root)
+            {
+                var scale = root.RasterizationScale > 0 ? root.RasterizationScale : 1;
+                if (AppWindow.Presenter is OverlappedPresenter overlappedPresenter)
+                {
+                    overlappedPresenter.PreferredMinimumWidth = (int)(250 * scale);
+                    overlappedPresenter.PreferredMinimumHeight = (int)(250 * scale);
+                }
             }
         }
     }
