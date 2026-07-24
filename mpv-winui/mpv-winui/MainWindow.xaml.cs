4d7dc8bf6c7a95e72872c23c69e834cf6c6dc935
ikas <65895428+ikas-mc@users.noreply.github.com>
2026-07-24 22:05:07 +0800

    fix fullscreen size

--- mpv-winui/mpv-winui/MainWindow.xaml.cs
+++ mpv-winui/mpv-winui/MainWindow.xaml.cs
@@ -19,12 +19,11 @@ namespace mpv_winui
 
             TrySetBackdrop();
 
-            ExtendsContentIntoTitleBar = true;
+            AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
             ShellTitleBar.Title = PackageHelper.AppName;
             SetTitleBarColors();
             SetTitleBar(ShellTitleBar);
 
-            AppWindow.Changed += AppWindow_Changed;
             AppWindow.Title = PackageHelper.AppName;
             AppWindow.SetIcon("App.ico");
 
@@ -35,66 +34,24 @@ namespace mpv_winui
         {
             if (AppWindowTitleBar.IsCustomizationSupported())
             {
-                AppWindowTitleBar m_TitleBar = AppWindow.TitleBar;
+                AppWindowTitleBar titleBar = AppWindow.TitleBar;
 
-                // Set active window colors.
-                // Note: No effect when app is running on Windows 10
-                // because color customization is not supported.
-                m_TitleBar.ForegroundColor = Colors.White;
-                //m_TitleBar.BackgroundColor = Colors.Green;
-                m_TitleBar.ButtonForegroundColor = Colors.White;
-                //m_TitleBar.ButtonBackgroundColor = Colors.SeaGreen;
-                //m_TitleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
-                //m_TitleBar.ButtonHoverBackgroundColor = Colors.DarkSeaGreen;
-                //m_TitleBar.ButtonPressedForegroundColor = Colors.Gray;
-                //m_TitleBar.ButtonPressedBackgroundColor = Colors.LightGreen;
+                titleBar.ForegroundColor = Colors.White;
+                //titleBar.BackgroundColor = Colors.Green;
+                titleBar.ButtonForegroundColor = Colors.White;
+                titleBar.ButtonBackgroundColor = Colors.Transparent;
+                //titleBar.ButtonHoverForegroundColor = Colors.Gainsboro;
+                //titleBar.ButtonHoverBackgroundColor = Colors.Transparent;
+                //titleBar.ButtonPressedForegroundColor = Colors.Gray;
+                //titleBar.ButtonPressedBackgroundColor = Colors.LightGreen;
 
-                // Set inactive window colors.
-                // Note: No effect when app is running on Windows 10
-                // because color customization is not supported.
-                //m_TitleBar.InactiveForegroundColor = Colors.Gainsboro;
-                //m_TitleBar.InactiveBackgroundColor = Colors.SeaGreen;
-                //m_TitleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
-                //m_TitleBar.ButtonInactiveBackgroundColor = Colors.SeaGreen;
+                //titleBar.InactiveForegroundColor = Colors.Gainsboro;
+                //titleBar.InactiveBackgroundColor = Colors.SeaGreen;
+                //titleBar.ButtonInactiveForegroundColor = Colors.Gainsboro;
+                titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
                 return true;
             }
             return false;
-        }
-
-        //https://learn.microsoft.com/en-us/windows/apps/develop/title-bar
-        private void AppWindow_Changed(AppWindow sender, AppWindowChangedEventArgs args)
-        {
-            if (args.DidPresenterChange)
-            {
-                switch (sender.Presenter.Kind)
-                {
-                    case AppWindowPresenterKind.CompactOverlay:
-                        // Compact overlay - hide custom title bar
-                        // and use the default system title bar instead.
-                        ShellTitleBar.Visibility = Visibility.Collapsed;
-                        sender.TitleBar.ResetToDefault();
-                        break;
-
-                    case AppWindowPresenterKind.FullScreen:
-                        // Full screen - hide the custom title bar
-                        // and the default system title bar.
-                        ShellTitleBar.Visibility = Visibility.Collapsed;
-                        sender.TitleBar.ExtendsContentIntoTitleBar = true;
-                        break;
-
-                    case AppWindowPresenterKind.Overlapped:
-                        // Normal - hide the system title bar
-                        // and use the custom title bar instead.
-                        ShellTitleBar.Visibility = Visibility.Visible;
-                        sender.TitleBar.ExtendsContentIntoTitleBar = true;
-                        break;
-
-                    default:
-                        // Use the default system title bar.
-                        sender.TitleBar.ResetToDefault();
-                        break;
-                }
-            }
         }
 
         public async void Open()
@@ -111,7 +68,6 @@ namespace mpv_winui
             }
 
             ShellFrame.Navigate(typeof(MpvPlayerPage), fileItems);
-
         }
 
         public async void Refresh(AppActivationArguments activatedArgs)
