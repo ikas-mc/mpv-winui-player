e52a12dfff82503c977f651a42a3ae8a9b281be8
ikas <65895428+ikas-mc@users.noreply.github.com>
2026-08-14 13:38:26 +0800

    refactor settings view

--- mpv-winui/mpv-winui/Modules/Settings/SettingsPage.xaml.cs
+++ mpv-winui/mpv-winui/Modules/Settings/SettingsPage.xaml.cs
@@ -27,6 +27,10 @@ public sealed partial class SettingsPage : Page
             {
                 Key = nameof(AppContext.AppSetting.ThemeType),
                 Label = "Theme",
+                Description = "App theme",
+                Icon = "\uE790",
+                GroupKey="Style",
+                GroupLabel="Style",
                 Type = OptionType.StringList,
                 Options = [AppSettings.ThemeType_Auto, AppSettings.ThemeType_Light, AppSettings.ThemeType_Dark],
                 Getter = () => AppContext.AppSetting.ThemeType,
@@ -39,7 +43,10 @@ public sealed partial class SettingsPage : Page
             new Option
             {
                 Key =  nameof(AppContext.AppSetting.BackdropType),
-                Label = "Backdrop (Restart Required)",
+                Label = "Backdrop",
+                Description = "Window background. Restart required.",
+                GroupKey="Style",
+                GroupLabel="Style",
                 Type = OptionType.StringList,
                 Options = [AppSettings.BackdropType_Acrylic, AppSettings.BackdropType_Mica],
                 Getter = () => AppContext.AppSetting.BackdropType,
@@ -48,17 +55,12 @@ public sealed partial class SettingsPage : Page
 
             new Option
             {
-                Key =  nameof(AppContext.AppSetting.EnableDebugLog),
-                Label = "Debug Log (Restart Required)",
-                Type = OptionType.Boolean,
-                Getter = () => AppContext.AppSetting.EnableDebugLog,
-                Setter = v => AppContext.AppSetting.EnableDebugLog = (bool)v!
-            },
-
-            new Option
-            {
                 Key =  nameof(AppContext.AppSetting.EnableVideoPreview),
-                Label = "Preview (Restart Required)",
+                Label = "Thumbnails Preview",
+                Description = "Using a plugin that supports the mpv osc-preview-api.",
+                Icon = "\uE8B2",
+                GroupKey="Preview",
+                GroupLabel="Preview",
                 Type = OptionType.Boolean,
                 Getter = () => AppContext.AppSetting.EnableVideoPreview,
                 Setter = v => AppContext.AppSetting.EnableVideoPreview = (bool)v!
@@ -67,7 +69,11 @@ public sealed partial class SettingsPage : Page
             new Option
             {
                 Key =  nameof(AppContext.AppSetting.EnableVideoBuiltInPreview),
-                Label = "Built-in Preview (Restart Required)",
+                Label = "Built-in Thumbnails Preview",
+                Description = "Built-in preview. Restart required.",
+                Icon = "\uE8B2",
+                GroupKey="Preview",
+                GroupLabel="Preview",
                 Type = OptionType.Boolean,
                 Getter = () => AppContext.AppSetting.EnableVideoBuiltInPreview,
                 Setter = v => AppContext.AppSetting.EnableVideoBuiltInPreview = (bool)v!
@@ -76,7 +82,11 @@ public sealed partial class SettingsPage : Page
             new Option
             {
                 Key =  nameof(AppContext.AppSetting.KeepVideoBuiltInPreviewAlive),
-                Label = "Built-in Preview Keep Alive (Restart Required)",
+                Label = "Built-in Preview Keep Alive",
+                Description = "Keep the preview mpv instance alive. Restart required.",
+                Icon = "\uE8B2",
+                GroupKey="Preview",
+                GroupLabel="Preview",
                 Type = OptionType.Boolean,
                 Getter = () => AppContext.AppSetting.KeepVideoBuiltInPreviewAlive,
                 Setter = v => AppContext.AppSetting.KeepVideoBuiltInPreviewAlive = (bool)v!
@@ -84,14 +94,31 @@ public sealed partial class SettingsPage : Page
 
             new Option
             {
-                Key = nameof(AppContext.AppSetting.BuiltInPreviewAliveTimeout),
-                Label = "Built-in Preview Alive Timeout(s) (Restart Required)",
+                Key =  nameof(AppContext.AppSetting.BuiltInPreviewAliveTimeout),
+                Label = "Built-in Preview Alive Timeout (s)",
+                Description = "Seconds the preview mpv instance stays alive.",
+                Icon = "\uE8B2",
+                GroupKey="Preview",
+                GroupLabel="Preview",
                 Type = OptionType.Integer,
                 Min = 0,
                 Max = int.MaxValue,
                 Step = 10,
                 Getter = () => AppContext.AppSetting.BuiltInPreviewAliveTimeout,
                 Setter = v => AppContext.AppSetting.BuiltInPreviewAliveTimeout = (int)v!
+            },
+
+            new Option
+            {
+                Key =  nameof(AppContext.AppSetting.EnableDebugLog),
+                Label = "Debug Log",
+                Description = "Enable app and mpv debug log. Restart required.",
+                Icon = "\uE946",
+                GroupKey="Data",
+                GroupLabel="Data",
+                Type = OptionType.Boolean,
+                Getter = () => AppContext.AppSetting.EnableDebugLog,
+                Setter = v => AppContext.AppSetting.EnableDebugLog = (bool)v!
             },
         ];
     }
